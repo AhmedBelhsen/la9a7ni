dd6b4d8194758c58cfbcb1602b646c6bbc82002c
rouu123 <158215276+rouu123@users.noreply.github.com>
2024-08-23 17:10:43 +0100

    added back button

--- src/pages/FindHospital.jsx
+++ src/pages/FindHospital.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import HospitalCard from "../components/HospitalCard";
 import useDoctorCurrentLocation from '../hooks/useDoctorCurrentLocation';
+import { useNavigate } from "react-router-dom";
+import { Button } from 'react-bootstrap'
 
 const FindHospital = () => {
     const { doctors } = useDoctorCurrentLocation('hospitals');
+    const navigate = useNavigate();
+    const goBack=()=>{
+        navigate("/");
+
+    }
 
     
     return (
-        <div>
-            <h1>hospitals in your area</h1>
+        <div style={{margin:15}}>
+          <button onClick={goBack} className="back-button" >Back</button>
+
+            <h1>   Hospitals in your area:</h1>
             {doctors.map((hospital) => (
                 <HospitalCard key={hospital.id} hospital={hospital} />
             ))}
--- src/pages/FindVet.jsx
+++ src/pages/FindVet.jsx
@@ -1,17 +1,23 @@
 import React from "react";
 import VetCard from "../components/VetCard";
 import useDoctorCurrentLocation from '../hooks/useDoctorCurrentLocation';
+import { useNavigate } from "react-router-dom";
 
 const FindVet = () => {
     const { doctors } = useDoctorCurrentLocation('vets');
+    const navigate = useNavigate();
+    const goBack=()=>{
+        navigate("/");
 
+    }
     return (
-        <div>
-            <h1>Vets in your area</h1>
+        <div style={{margin:15}}>
+ <button onClick={goBack} className="back-button" >Back</button>         
+    <h1>   Vets in your area:</h1>
             {doctors.map((vet) => (
                 <VetCard key={vet.id} vet={vet} />
             ))}
-            <h1>Get your pet vaccinated</h1>
+            <h1>  Get your pet vaccinated! </h1>
  
         </div>
     );
